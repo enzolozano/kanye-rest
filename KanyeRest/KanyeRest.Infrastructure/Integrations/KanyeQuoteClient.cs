e4eb277ce8b15656c8ed1486ecb73a594d392bff
Enzo <enzo.malozano@gmail.com>
2025-05-19 09:33:19 -0300

    Add service and client for random quotes

---
+++ KanyeRest/KanyeRest.Infrastructure/Integrations/KanyeQuoteClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace KanyeRest.Infrastructure.Integrations
+{
+    public class KanyeQuoteClient : IKanyeQuoteClient
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly string _urlKanyeWestQuoteApi = "https://api.kanye.rest/";
+
+        public KanyeQuoteClient() 
+        { 
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetRandomQuoteAsync()
+        {
+            var response = await _httpClient.GetStringAsync(_urlKanyeWestQuoteApi);
+            var json = JsonDocument.Parse(response);
+
+            return json.RootElement
+                .GetProperty("quote")
+                .ToString();
+        }
+    }
+}
